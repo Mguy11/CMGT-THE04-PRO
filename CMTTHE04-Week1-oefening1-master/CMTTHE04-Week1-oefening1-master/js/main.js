c7f413a3850be1422ae0917d68177835c2615ed3
Mguy11 <0911554@hr.nl>
2018-04-30 22:30:13 +0200

    Comments toegevoegd aan de code

--- CMTTHE04-Week1-oefening1-master/CMTTHE04-Week1-oefening1-master/js/main.js
+++ CMTTHE04-Week1-oefening1-master/CMTTHE04-Week1-oefening1-master/js/main.js
@@ -1,7 +1,3 @@
-//
-// geef de vis een random positie en een random kleur
-//
-
 
 let screenWidth = window.innerWidth - 130
 let screenHeight = window.innerHeight - 110
@@ -12,8 +8,11 @@ let timeoutID1
 let timeoutID2
 let timeoutID3
 
+//Added a counter to keep track of how many fishes are created per species
+
 let count1 = 0
 let count2 = 0
+
 // Creates a fish element with a random x and y value on your screen. They move up and down on your screen. The fishes themself also have
 // a different colour and a dying animation when clicked.
 
@@ -21,29 +20,26 @@ function upFish() {
 
     let fish = document.createElement('fish')
     document.body.appendChild(fish)
-    
+
     let randomNumber = Math.random() * screenWidth
     let randomNumber2 = Math.random() * screenHeight
-    
-    if((randomNumber2 < screenHeight)&&(randomNumber < screenWidth)){
-         fish.style.top = randomNumber2 + "px"
-         fish.style.left = randomNumber + "px"
-    }
-    else{
 
-    }  
+    if ((randomNumber2 < screenHeight) && (randomNumber < screenWidth)) {
+        fish.style.top = randomNumber2 + "px"
+        fish.style.left = randomNumber + "px"
+    }
+
 
     fish.style.webkitFilter = "hue-rotate(" + Math.floor((Math.random() * 360)) + "deg)"
-    fish.style.filter = "hue-rotate(" + Math.floor((Math.random() * 360)) + "deg)" 
+    fish.style.filter = "hue-rotate(" + Math.floor((Math.random() * 360)) + "deg)"
 
-    fish.addEventListener("click", function () 
-{
-    fish.classList.add("dead")
-    deadfish++
-});
-    timeoutID1 = setTimeout(function() {upFish()}, 2000);
+    fish.addEventListener("click", function () {
+        fish.classList.add("dead")
+        count1--
+    });
+    timeoutID1 = setTimeout(function () { upFish() }, 2000);
 
-    if(count1 === 50){
+    if (count1 === 50) {
         clearTimeout(timeoutID1)
     }
 
@@ -55,84 +51,67 @@ function upFish() {
 // Creates a fish element with a random x and y value on your screen. They move left to right on your screen. The fishes themself also have
 // a different colour and a dying animation when clicked.
 
-function rightFish()
-{   
-    
-        let fish2 = document.createElement('fish2')
-        document.body.appendChild(fish2)
+function rightFish() {
 
-        let randomNumber = Math.random() * screenWidth
-        let randomNumber2 = Math.random() * screenHeight
-        
-        if((randomNumber2 < screenHeight) && (randomNumber < screenWidth)){
-             fish2.style.top = randomNumber2 + "px"
-             fish2.style.left = randomNumber + "px"
-        }
-        else{
-    
-        }
-        
-        fish2.style.webkitFilter = "hue-rotate(" + Math.floor((Math.random() * 360)) + "deg)"
-        fish2.style.filter = "hue-rotate(" + Math.floor((Math.random() * 360)) + "deg)"
+    let fish2 = document.createElement('fish2')
+    document.body.appendChild(fish2)
 
-        fish2.addEventListener("click", function()
-        {
-            fish2.classList.add("dead") 
-            deadfish++
-        });
-       timeoutID2 = setTimeout(function() {rightFish()}, 2000);
+    let randomNumber = Math.random() * screenWidth
+    let randomNumber2 = Math.random() * screenHeight
 
-       if(count2 === 50){
+    if ((randomNumber2 < screenHeight) && (randomNumber < screenWidth)) {
+        fish2.style.top = randomNumber2 + "px"
+        fish2.style.left = randomNumber + "px"
+    }
+
+
+    fish2.style.webkitFilter = "hue-rotate(" + Math.floor((Math.random() * 360)) + "deg)"
+    fish2.style.filter = "hue-rotate(" + Math.floor((Math.random() * 360)) + "deg)"
+
+    fish2.addEventListener("click", function () {
+        fish2.classList.add("dead")
+        count2--
+    });
+    timeoutID2 = setTimeout(function () {
+        rightFish()
+    }, 2000);
+
+    if (count2 === 50) {
         clearTimeout(timeoutID2)
     }
-    
-       count2++
 
-       console.log(count2)
+    count2++
+
+    console.log(count2)
 }
-//
-// geef de bubble een random positie
-//
 
 // The bubbles are spawned with a random position on your screen, they also have an animation to float upwards on your screen.
 
-function bubbleCreate()
-{
-    
+function bubbleCreate() {
+
     let bubble = document.createElement('bubble')
     document.body.appendChild(bubble)
-    
+
     bubble.style.top = Math.random() * screenHeight + "px"
     bubble.style.left = Math.random() * screenWidth + "px"
-    
-    timeoutID3 = setTimeout(function() {bubbleCreate()}, 1000);
-    
+
+    timeoutID3 = setTimeout(function () {
+        bubbleCreate()
+    }, 1000);
+
 }
 
-//
+//Added a function to run all the funtions at once to create the "Aquarium"
+function aquarium() {
 
-
-function aquarium(){
-   
     upFish();
     rightFish();
     bubbleCreate();
 
- 
 }
-//
-// roep een functie aan als alles geladen is
-//
-if(count1 === 10 && count2 === 10){
-    clearTimeout(timeoutID1);
-    clearTimeout(timeoutID2);
-    clearTimeout(timeoutID3);
-}
-else{
-    aquarium();
-}
-/*window.addEventListener("load", function ()
+
+window.addEventListener("load", function ()
 {
     console.log("start het aquarium")
     
-})*/
+})
