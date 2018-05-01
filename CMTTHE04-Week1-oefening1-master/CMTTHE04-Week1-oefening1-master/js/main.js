d9f4de12a5e22981a5c692349f12ecda8279b5cc
Mguy11 <0911554@hr.nl>
2018-05-01 18:11:06 +0200

    Opdracht leuker gemaakt met haaien!!!

--- CMTTHE04-Week1-oefening1-master/CMTTHE04-Week1-oefening1-master/js/main.js
+++ CMTTHE04-Week1-oefening1-master/CMTTHE04-Week1-oefening1-master/js/main.js
@@ -2,16 +2,19 @@
 let screenWidth = window.innerWidth - 130
 let screenHeight = window.innerHeight - 110
 
-let deadfish
+
 
 let timeoutID1
 let timeoutID2
 let timeoutID3
+let timeoutID4
+let timeoutID5
 
 //Added a counter to keep track of how many fishes are created per species
 
 let count1 = 0
 let count2 = 0
+let count3 = 0
 
 // Creates a fish element with a random x and y value on your screen. They move up and down on your screen. The fishes themself also have
 // a different colour and a dying animation when clicked.
@@ -35,8 +38,11 @@ function upFish() {
 
     fish.addEventListener("click", function () {
         fish.classList.add("dead")
-        count1--
+        if(fish.style.animationName === 'died'){
+            console.log('hooi')
+        }
     });
+
     timeoutID1 = setTimeout(function () { upFish() }, 2000);
 
     if (count1 === 50) {
@@ -85,6 +91,41 @@ function rightFish() {
     console.log(count2)
 }
 
+function sharkLeft() {
+    let shark1 = document.createElement('shark1')
+    document.body.appendChild(shark1)
+
+    let randomNumber = Math.random() * screenWidth
+    let randomNumber2 = Math.random() * screenHeight
+    let randomNumber3 = Math.floor((Math.random() * 5))
+
+    
+        shark1.style.top = ((screenHeight / 5) * randomNumber3) + "px"
+        shark1.style.right = screenWidth  + 300 + "px"
+    
+        timeoutID4 = setTimeout(function () { sharkLeft() }, 8000);
+}
+
+function sharkRight() {
+    let shark2 = document.createElement('shark2')
+    document.body.appendChild(shark2)
+
+    let randomNumber = Math.random() * screenWidth
+    let randomNumber2 = Math.random() * screenHeight
+    let randomNumber3 = Math.floor((Math.random() * 5))
+
+        
+        shark2.style.top = ((screenHeight / 5) * randomNumber3) + "px"
+        shark2.style.left = "-300px"
+
+        timeoutID5 = setTimeout(function () { sharkRight() }, 8000);
+    
+}
+
+function sharkSwim(){
+    sharkLeft()
+    sharkRight()
+}
 // The bubbles are spawned with a random position on your screen, they also have an animation to float upwards on your screen.
 
 function bubbleCreate() {
@@ -98,8 +139,12 @@ function bubbleCreate() {
     timeoutID3 = setTimeout(function () {
         bubbleCreate()
     }, 1000);
+    if (count1 === 100) {
+        clearTimeout(timeoutID3)
+    }
 
 }
+
 
 //Added a function to run all the funtions at once to create the 
 function aquarium() {
@@ -107,8 +152,10 @@ function aquarium() {
     upFish();
     rightFish();
     bubbleCreate();
+    sharkSwim();
 
 }
+
 
 
 aquarium();
